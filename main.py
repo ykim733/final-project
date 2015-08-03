0640ed56646e7951501e8be7df70cd29686ef3e1
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-03 14:32:25 -0500

    data store is semi working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-
+from google.appengine.ext import ndb
 import webapp2
 import jinja2
 import os
@@ -7,18 +7,27 @@ import os
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+class Productivity(ndb.Model):
+    essay = ndb.StringProperty(required = False)
+    username = ndb.StringProperty(required = True)
+    password = ndb.StringProperty(required = False)
+
+
+
+# Hai girl hai, so I got the user input working in the url input, but I haven't been able to link it in a form good luck!
+# btw I just added a username input in the form with the essay entitle from.html
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+            first_template = jinja_environment.get_template('form.html') #this isn't working
+            user = Productivity(username=self.request.get("user"))
+            user.put()
+            qry = Productivity.query(Productivity.username == "nicki").fetch()
+            self.response.out.write(first_template.render(user)) #this isn't working either
+            self.response.out.write(qry)
 
-        userlogin = True
-        if userlogin:
-            self.response.out.write("Welcome!")
-        else:
-            self.response.out.write("Please login")
-        template = jinja_environment.get_template('form.html')
-        self.response.write(template.render())
 
-    self.request.get(essay_text)
+
 
 
 

feb115c651d96c4f842a804912a92f2c494f2fae
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-03 11:32:26 -0500

    main update

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+
+
 import webapp2
 import jinja2
 import os
 
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+      self.response.out.write("Hello world!")
+    
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)

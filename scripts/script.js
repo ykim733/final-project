a15e01dede82468e3005d7af7b10f5839e9d20da
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-05 09:49:10 -0500

    timer update

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,37 @@
-function enableTab(id) {
-    var el = document.getElementById(id);
-    el.onkeydown = function(e) {
-        if (e.keyCode === 9) { // tab was pressed
+$(document).ready(function() {
+  function enableTab(id) {
+      var el = document.getElementById(id);
+      el.onkeydown = function(e) {
+          if (e.keyCode === 9) { // tab was pressed
 
-            // get caret position/selection
-            var val = this.value,
-                start = this.selectionStart,
-                end = this.selectionEnd;
+              // get caret position/selection
+              var val = this.value,
+                  start = this.selectionStart,
+                  end = this.selectionEnd;
 
-            // set textarea value to: text before caret + tab + text after caret
-            this.value = val.substring(0, start) + '\t' + val.substring(end);
+              // set textarea value to: text before caret + tab + text after caret
+              this.value = val.substring(0, start) + '\t' + val.substring(end);
 
-            // put caret at right position again
-            this.selectionStart = this.selectionEnd = start + 1;
+              // put caret at right position again
+              this.selectionStart = this.selectionEnd = start + 1;
 
-            // prevent the focus lose
-            return false;
+              // prevent the focus lose
+              return false;
 
-        }
-    };
-}
+          }
+      };
+  }
 
-// Enable the tab character onkeypress (onkeydown) inside textarea...
-// ... for a textarea that has an `id="my-textarea"`
-enableTab('essay');
+  // Enable the tab character onkeypress (onkeydown) inside textarea...
+  // ... for a textarea that has an `id="my-textarea"`
+  enableTab('essay');
 
-function bold() {
-    var x = $('#description-box');
-    if (x.css("font-weight") !== "bold") {
-        x.css("font-weight", "bold");
-    } else {
-        x.css("font-weight", "normal");
-    }
+  function bold() {
+      var x = $('#description-box');
+      if (x.css("font-weight") !== "bold") {
+         (x.css("font-weight", "bold"))
+       else 
+          (x.css("font-weight", "normal");
+      }
+  }
 }
